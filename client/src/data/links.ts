f85c343b571675a27def2d2ac0b6a4cee9b0c40a
Kevon Kibochi <91994339+devokrann@users.noreply.github.com>
2024-01-30 17:42:38 +0300

    Globalized navbar links

---
+++ client/src/data/links.ts
@@ -0,0 +1,27 @@
+import image from "@src/assets/images";
+
+const links = {
+	social: [
+		{
+			icon: image.icon.social.facebook,
+			link: "https://www.instagram.com/aiconference_Ke/",
+		},
+		{
+			icon: image.icon.social.twitter,
+			link: "https://x.com/aiconference_ke?t=vH9rLLLgruVtU64zJy34vA&s=09",
+		},
+		{
+			icon: image.icon.social.instagram,
+			link: "https://www.instagram.com/aiconference_ke?igsh=cjRheXZzdDc1N2Fw",
+		},
+	],
+	navbar: [
+		{ link: "/", label: "Overview" },
+		{ link: "/exhibit", label: "Exhibit" },
+		{ link: "/agenda", label: "Agenda" },
+		{ link: "/speakers", label: "Speakers" },
+		{ link: "/sponsors", label: "Sponsors" },
+	],
+};
+
+export default links;
