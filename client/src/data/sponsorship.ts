42c2ebca83fbcf6ff5f07302b8eed03bab3ce2bd
Kevon Kibochi <91994339+devokrann@users.noreply.github.com>
2024-01-30 17:42:27 +0300

    Added sponsorship packages

---
+++ client/src/data/sponsorship.ts
@@ -0,0 +1,100 @@
+const sponsorship = {
+	packages: [
+		{
+			title: "Title",
+			price: "6,000,000",
+			desc: [
+				"Exclusive branding as the title sponsor of the event",
+				"Prominent logo placement on all promotional materials, including digital and print",
+				"Recognition in press releases and media coverage",
+				"Opportunity to deliver a keynote or exclusive presentation (15 mins)",
+				"5 Give aways for panel discussion speakers pack/bag",
+				"5 banners in both the conference and exhibition areas",
+				"Booth Space in the exhibition area 12 by 3.",
+				"Conference Pass for 15 pax",
+				"Access to VIP Coffee Break Area",
+				"Lanyard Branding of logo",
+				"Exclusive access to event data analytics for post-event analysis.",
+			],
+		},
+		{
+			title: "Platinum",
+			price: "4,000,000",
+			desc: [
+				"High visibility with prominent logo placement on event materials.",
+				"Recognition in press releases and media coverage.",
+				"Opportunity to host a workshop or panel discussion on AI, Drone Tech, or",
+				"Data Analytics.",
+				"X3 Give aways for panel discussion speakers pack/bag",
+				"3 banners in both the conference and exhibition areas",
+				"Booth Space in the exhibition area 9 by 3.",
+				"Conference Pass for 12 pax",
+				"Access to VIP Coffee Break Area",
+				"Lanyard Branding of logo",
+				"Priority access to event data analytics for post-event analysis",
+			],
+		},
+		{
+			title: "Gold",
+			price: "2,500,000",
+			desc: [
+				"Logo placement on event materials, including digital and print.",
+				"Recognition in press releases and media coverage.",
+				"2 banners in both the conference and exhibition areas",
+				"Booth Space in the exhibition area 6 by 3.",
+				"Conference pass for 10 pax",
+				"Mention in keynote or opening remarks.",
+				"Access to VIP Coffee Break Area",
+				"Access to event data analytics for post-event analysis.",
+			],
+		},
+		{
+			title: "Silver",
+			price: "1,000,000",
+			desc: [
+				"Logo placement on event materials, including digital and print.",
+				"Recognition in press releases and media coverage.",
+				"1 banner in both the conference and exhibition areas",
+				"Exhibit space in the sponsor area 6 by 3.",
+				"Conference pass for 8 pax",
+				"Mention in event communications.",
+				"Access to VIP Coffee Break Area",
+				"Access to event data analytics for post-event analysis.",
+			],
+		},
+		{
+			title: "Bronze",
+			price: "500,000",
+			desc: [
+				"Logo placement on event materials, including digital and print.",
+				"Recognition in press releases and media coverage.",
+				"Standard exhibit space 3 by 3.",
+				"Conference pass for 3 pax",
+				"Mention in event communications.",
+				"Access to VIP Coffee Break Area",
+			],
+		},
+		{
+			title: "Coffee Break",
+			price: "500,000",
+			desc: [
+				"Exclusive sponsorship of coffee break sessions.",
+				"Logo on coffee stations and materials.",
+				"Recognition in event communications.",
+				"Opportunity to distribute branded materials during coffee breaks.",
+			],
+		},
+		{
+			title: "5G Wi-Fi",
+			price: "500,000",
+			desc: [
+				"Exclusive sponsorship of event Wi-Fi.",
+				"Logo on Wi-Fi login pages and materials.",
+				"Recognition in event communications.",
+				"Opportunity to provide branded splash pages or login messages.",
+			],
+		},
+	],
+};
+
+export default sponsorship;
