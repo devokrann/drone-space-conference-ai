7c17238696c36e0bbeda5832bb369720111710d1
Kevon Kibochi <91994339+devokrann@users.noreply.github.com>
2024-01-30 17:43:21 +0300

    Added main header

---
+++ client/src/partials/headers/Main.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+import { Box, Container, Flex, Group, Image, Text } from "@mantine/core";
+
+import { IconPointFilled } from "@tabler/icons-react";
+
+import classes from "./Main.module.scss";
+
+import data from "@src/data";
+
+export default function Main() {
+	return (
+		<Box className={classes.box}>
+			<Container>
+				<Flex direction={{ base: "column", xs: "row" }} align={"center"} justify="space-between" gap={"sm"}>
+					<Flex align={"center"} direction={{ base: "column", xs: "row" }} gap={"xs"}>
+						<Text inherit>26 - 27 March, 2024</Text>
+						<Text inherit>Sarit expo Centre, Westlands</Text>
+					</Flex>
+					<Group>
+						<Group gap={"xs"}>
+							{data.links.social.map(link => (
+								<Link to={link.link}>
+									<Image src={link.icon} w={24} />
+								</Link>
+							))}
+						</Group>
+					</Group>
+				</Flex>
+			</Container>
+		</Box>
+	);
+}
