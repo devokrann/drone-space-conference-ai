e0f2273d799051a19303f30a9384a9eda59f815a
Kevon Kibochi <91994339+devokrann@users.noreply.github.com>
2024-01-30 17:41:51 +0300

    Updated css to scss

--- client/src/components/breadcrumbs/Hero.tsx
+++ client/src/components/breadcrumbs/Hero.tsx
@@ -5,22 +5,18 @@ import { Box, Breadcrumbs } from "@mantine/core";
 
 import { IconChevronRight } from "@tabler/icons-react";
 
-import classes from "./Hero.module.css";
+import classes from "./Hero.module.scss";
 
-export default function Hero({
-	data,
-}: {
-	data: { label: string; link: string }[];
-}) {
+export default function Hero({ data }: { data: { label: string; link: string }[] }) {
 	return (
-		<Breadcrumbs separator={<IconChevronRight size={12} stroke={2} />}>
-			{data.map((crumb) => (
+		<Breadcrumbs separator={<IconChevronRight color="var(--mantine-color-white)" size={12} stroke={2} />}>
+			{data.map(crumb => (
 				<Box
 					component={Link}
 					to={crumb.link}
 					key={crumb.link}
 					className={classes.link}
-					c={data.indexOf(crumb) == data.length - 1 ? "pri.8" : ""}
+					// c={data.indexOf(crumb) == data.length - 1 ? "pri.8" : ""}
 				>
 					{crumb.label}
 				</Box>
