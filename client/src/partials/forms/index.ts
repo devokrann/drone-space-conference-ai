2850d275bb9ab3613fa7a8633dece65c7660041f
Kevon Kibochi <91994339+devokrann@users.noreply.github.com>
2024-02-02 12:45:13 +0300

    Add forms

--- client/src/partials/forms/index.ts
+++ client/src/partials/forms/index.ts
@@ -1,9 +1,11 @@
+import Booth from "./Booth";
 import Change from "./Change";
 import Contact from "./Contact";
 import Login from "./Login";
 import Newsletter from "./Newsletter";
 import Reset from "./Reset";
 import Signup from "./Signup";
+import Sponsorship from "./Sponsorship";
 import Verify from "./Verify";
 
 const form = {
@@ -13,6 +15,10 @@ const form = {
 	Reset: Reset,
 	Change: Change,
 	Contact: Contact,
+
+	Booth: Booth,
+	Sponsorship: Sponsorship,
+
 	// Newsletter: Newsletter,
 };
 export default form;
