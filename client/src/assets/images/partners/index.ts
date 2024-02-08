3697fe152cfdfb15e98b704fb1139ec65d064035
Kevon Kibochi <91994339+devokrann@users.noreply.github.com>
2024-02-08 18:56:38 +0300

    Add partner

--- client/src/assets/images/partners/index.ts
+++ client/src/assets/images/partners/index.ts
@@ -1,7 +1,13 @@
 import media from "./media";
 
+import partner1 from "./partner-1.jpg";
+
 const partners = {
 	media: media,
+
+	other: {
+		partner1: partner1,
+	},
 };
 
 export default partners;
