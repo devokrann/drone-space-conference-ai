bcfa03a851d4efcaddddfc39d63e25c16e8751ea
Kevon Kibochi <91994339+devokrann@users.noreply.github.com>
2024-02-26 09:00:15 +0300

    Update files

---
+++ client/src/routes/hackathon/index.tsx
@@ -0,0 +1,77 @@
+import React from "react";
+import { useLoaderData } from "react-router-dom";
+
+import Layout from "@src/layouts";
+import Partial from "@src/partials";
+import { Divider, Grid, List, Stack, Text, Title } from "@mantine/core";
+import data from "@src/data";
+import Component from "@src/components";
+
+// export async function loader() {
+// 	return "data";
+// }
+
+const tc = [
+	"Eligibility: The hackathon is open to individuals who are at least 18 years old and will be physically in Nairobi on the 26th and 27th of March 2024.",
+	"Team Formation : Participants may form teams of up to 4 members. Each team must designate a team leader who will be the primary point of contact.",
+	"Registration: All participants must register for the hackathon through the official registration process. Registrations made after the specified deadline will not be considered.",
+	"Original Work: All projects submitted must be original work. Plagiarism or unauthorized use of third-party intellectual property is strictly prohibited.",
+	"Code of Conduct: Participants are expected to conduct themselves in a professional and respectful manner throughout the hackathon. Any form of harassment or discrimination will not be tolerated and may result in immediate disqualification.",
+	"Judging Criteria: Projects will be judged based on criteria such as creativity, technical complexity, innovation, and overall presentation. The decisions of the judges are final and binding.",
+	"Prizes: Prizes will be awarded to the top-performing teams as determined by the judging panel. Prizes are non-transferable and may be subject to taxation in accordance with local laws.",
+	"Intellectual Property: Participants retain ownership of the intellectual property rights to their projects.",
+	"Final Participation: The organizers reserve the right to review the eligibility of participants and teams at any stage of the hackathon. Final participation and eligibility for prizes will be subject to review by a panel of judges.",
+	"Changes to Terms and Conditions: The organizers reserve the right to make changes to these terms and conditions at any time. Participants will be notified of any changes via email or through the official hackathon website.",
+	"Governing Law: These terms and conditions shall be governed by and construed in accordance with the laws of [insert governing jurisdiction]",
+];
+
+export default function Hackathon() {
+	// const data = useLoaderData();
+
+	return (
+		<Layout.Body header={<Partial.Header.Main />} nav={<Partial.Navbar.Main />} footer={<Partial.Footer.Main />}>
+			<Layout.Page hero={<Layout.Hero.Route />}>
+				<Layout.Section containerized margined>
+					<Stack ta={"center"} gap={"xl"}>
+						<Title order={2} fz={40} c={"pri"}>
+							Hackathon / Innovation Stage
+						</Title>
+						<Text>
+							This track is designed to showcase the industry's leading startups and provide an
+							opportunity for Venture Capitalists (VCs) on industry trends. All startups that will be
+							participating on this track are hand picked by Startinev and AI Conference Nairobi Team as
+							the up-and-comers to be on the lookout for. These sessions of Hackathon (building models)
+							and preparing presentations will be conducted in 48 hours and a showdown session of the
+							finalists will each conduct a 10 minute pitch style format of their idea with prize totals
+							of up to Kshs. 200,000.
+						</Text>
+					</Stack>
+				</Layout.Section>
+				<Divider />
+				<Layout.Section containerized containerSize="sm" padded bordered>
+					<Stack gap={"xl"}>
+						<Title order={3} ta={"center"} fz={32} c={"pri"}>
+							Hackathon Registration
+						</Title>
+						<Text ta={"center"} fw={500}>
+							Applications will undergo review and final hackathon participants will be contacted.
+						</Text>
+						<Partial.Form.Contact defaultInquiry="Hackathon Registration" />
+					</Stack>
+				</Layout.Section>
+				{/* <Layout.Section containerized margined>
+					<Stack gap={"xl"}>
+						<Title order={2} fz={40} c={"pri"} ta={"center"}>
+							Terms & Conditions
+						</Title>
+						<List size="sm" spacing={"md"} ta={"start"} listStyleType="ordered">
+							{tc.map(term => (
+								<List.Item key={term}>{term}</List.Item>
+							))}
+						</List>
+					</Stack>
+				</Layout.Section> */}
+			</Layout.Page>
+		</Layout.Body>
+	);
+}
